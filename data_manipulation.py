08d72364ff950b118670e2fd1d67f35f4661a1db
felipe-lim4 <felipe.vieira@school.school>
2025-03-12 09:46:49 -0300

    codigo de geração de grafico em barras

--- data_manipulation.py
+++ data_manipulation.py
@@ -15,6 +15,26 @@ df = pd.read_csv("titanic.csv")
 df.head()
 df["Survived"].plot
 
+# geração de grafico de barras para comparar quantidade de homens e mulheres que foram e que faleceram
+
+
+qtd_mulheres = len(df[df['Sex'] == 'female'])
+qtd_homens = len(df[df['Sex'] == 'male'])
+
+qtd_homens_falecidos= len( df[(df['Sex'] == 'male') & (df['Survived'] == 0)])
+qtd_mulheres_falecidas = len(df[(df['Sex'] == 'female') & (df['Survived'] == 0)])
+
+data = pd.DataFrame({
+    'Sexo': ['Mulheres', 'Homens', 'Mulheres falecidas', 'Homens falecidos'],
+    'Quantidade': [qtd_mulheres, qtd_homens, qtd_mulheres_falecidas, qtd_homens_falecidos]
+})
+
+data.plot(x = 'Sexo', y = 'Quantidade', kind='bar')
+
+plt.xlabel('Sexo')
+plt.ylabel('Quantidade')
+plt.title('Quantidade de Homens e Mulheres no Titanic')
+plt.show()
 
 
 
