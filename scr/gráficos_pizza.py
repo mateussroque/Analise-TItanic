39beeaf3b2ad2d3383bb5e74f25f364996cfff8f
felipe-lim4 <a939822@DESKTOP-2NRFB5U.>
2025-03-17 14:53:29 -0300

    adição de notebook para analise de dados

--- scr/gráficos_pizza.py
+++ scr/gráficos_pizza.py
@@ -84,32 +84,3 @@ data.plot.pie(y='Porcentagem', labels=data['Sexo'], autopct='%1.1f%%', startangl
 plt.ylabel('')
 plt.title('Porcentagem de Homens e Mulheres no Titanic')
 plt.show()
-
-df = pd.read_csv('titanic.csv')
-
-
-resultado = df.groupby(['Sex', 'Survived']).size().unstack(fill_value=0)
-
-
-resultado.columns = ['Falecidos', 'Sobreviventes']
-
-
-data = pd.DataFrame({
-    'Sexo': ['Mulheres sobreviventes', 'Homens sobreviventes', 'Mulheres falecidas', 'Homens falecidos'],
-    'Quantidade': [
-        resultado.loc['female', 'Sobreviventes'],
-        resultado.loc['male', 'Sobreviventes'],
-        resultado.loc['female', 'Falecidos'],
-        resultado.loc['male', 'Falecidos'],
-    ]
-})
-
-total = data['Quantidade'].sum()
-
-data['Porcentagem'] = (data['Quantidade'] / total) * 100
-
-data.plot.pie(y='Porcentagem', labels=data['Sexo'], autopct='%1.1f%%', startangle=90)
-
-plt.ylabel('')
-plt.title('Porcentagem de Homens e Mulheres no Titanic')
-plt.show()
